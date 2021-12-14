b63e3f1cd476931c33b5b8b0e61cbb65f79bf100
ufoo68 <mitarou.yuutarou@gmail.com>
2021-12-14 23:15:30 +0900

    たたき台の作成まで

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from '@/styles/Home.module.css'
+import Header from '@/components/Header'
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <Header />
+
+    </div>
+  )
+}
+
+export default Home
