6e15413da2195bf9457a98148a005d8cb34c1308
ufoo68 <mitarou.yuutarou@gmail.com]>
2022-04-29 19:01:05 +0900

    fix api key name

--- libs/microcms-client.ts
+++ libs/microcms-client.ts
@@ -2,5 +2,5 @@ import { createClient } from 'microcms-js-sdk';
 
 export const micrecmsClient = createClient({
   serviceDomain: 'ufoo68-portfolio',
-  apiKey: process.env.API_KEY,
+  apiKey: process.env.MICROCMS_API_KEY,
 });
