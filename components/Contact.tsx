fafdd6de969db825470c6e84f5ac48b6bc7185ff
ufoo68 <mitarou.yuutarou@gmail.com]>
2022-04-30 13:23:49 +0900

    微修正

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,9 +1,5 @@
 import { FormControl, Input, InputLabel, Box, TextareaAutosize, SxProps } from '@mui/material';
 
-const formItemStyle: SxProps = {
-  marginBottom: '10px',
-}
-
 const Contact = () => {
   return (
     <Box
@@ -15,23 +11,23 @@ const Contact = () => {
       }}
       component="form"
     >
-      <FormControl sx={formItemStyle}>
+      <FormControl margin="dense">
         <InputLabel aria-label="name" htmlFor="name">会社名もしくは個人名</InputLabel>
         <Input aria-label="name" id="name" />
       </FormControl>
-      <FormControl sx={formItemStyle}>
+      <FormControl margin="dense">
         <InputLabel aria-label="email" htmlFor="email">メールアドレス</InputLabel>
         <Input aria-label="email" id="email" />
       </FormControl>
-      <FormControl sx={formItemStyle}>
+      <FormControl margin="dense">
         <InputLabel aria-label="subject" htmlFor="subject">件名</InputLabel>
         <Input aria-label="subject" id="subject" />
       </FormControl>
-      <FormControl sx={formItemStyle}>
+      <FormControl margin="dense">
         <TextareaAutosize
           aria-label="text"
           placeholder="本文"
-          style={{ height: 300 }}
+          style={{ height: 200 }}
         />
       </FormControl>
     </Box>
